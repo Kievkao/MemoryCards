0289a23315dc67ef7a8925b0209775d7cb9ad8bc
Andrii Kravchenko <andrii.kravchenko@zalando.de>
2021-03-22 12:59:36 +0100

    Initial Commit

---
+++ MemoryCards/MemoryCardsApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryCardsApp.swift
+//  MemoryCards
+//
+//  Created by Andrii Kravchenko on 22.03.21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
